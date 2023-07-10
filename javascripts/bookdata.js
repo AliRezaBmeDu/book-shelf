7311e8f4f5f58e9c91f6a92c437c277dad786cc2
AliReza <ali.reza.buet@gmail.com>
2023-07-10 19:32:52 +0600

    Update the linters

--- javascripts/bookdata.js
+++ javascripts/bookdata.js
@@ -1,21 +1,23 @@
-export const bookDB = [
-    {
-      title: "Hamlet",
-      author: "Shakespear",
-    },
-    {
-      title: "The Outsider",
-      author: "Albert Camus",
-    },
-    {
-      title: "The Lord of the Rings",
-      author:"J.R.R. Tolkein"
-    },
-    {
-      title: "The Song of Ice and Fire",
-      author: "J.R. Martin"
-    }
-  ]
-  
-  
-  
+const bookDB = [
+  {
+    title: 'Hamlet',
+    author: 'Shakespear',
+  },
+  {
+    title: 'The Outsider',
+    author: 'Albert Camus',
+  },
+  {
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkein',
+  },
+  {
+    title: 'The Song of Ice and Fire',
+    author: 'J.R. Martin',
+  },
+];
+
+bookDB.push({
+  title: 'To Kill a Mockingbard',
+  author: 'Harper Lee',
+});
--- javascripts/script.js
+++ javascripts/script.js
@@ -1,79 +1,76 @@
 let bookDB = [
-    {
-      title: "Hamlet",
-      author: "Shakespeare",
-    },
-    {
-      title: "Deception Point",
-      author: "Dan Brown",
-    },
-    {
-      title: "The Lord of the Rings",
-      author: "J.R.R. Tolkien",
-    },
-    {
-      title: "The Song of Ice and Fire",
-      author: "George R.R. Martin",
-    },
-  ];
+  {
+    title: 'Hamlet',
+    author: 'Shakespeare',
+  },
+  {
+    title: 'Deception Point',
+    author: 'Dan Brown',
+  },
+  {
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    title: 'The Song of Ice and Fire',
+    author: 'George R.R. Martin',
+  },
+];
 
-  function retrieveBookDB() {
-    const storedBookDB = localStorage.getItem("bookDB");
-    if (storedBookDB) {
-      bookDB = JSON.parse(storedBookDB);
-    }
+function retrieveBookDB() {
+  const storedBookDB = localStorage.getItem('bookDB');
+  if (storedBookDB) {
+    bookDB = JSON.parse(storedBookDB);
   }
-  
-  function storeBookDB() {
-    localStorage.setItem("bookDB", JSON.stringify(bookDB));
-  }
-  
-  function createBookshelf() {
-    let bookshelf = document.getElementById("books");
-    bookshelf.innerHTML = "";
-  
-    for (let i = 0; i < bookDB.length; i += 1) {
-      let book = document.createElement("div");
-      book.classList.add("singlebook");
-      book.innerHTML = `
+}
+
+function storeBookDB() {
+  localStorage.setItem('bookDB', JSON.stringify(bookDB));
+}
+
+function createBookshelf() {
+  const bookshelf = document.getElementById('books');
+  bookshelf.innerHTML = '';
+
+  for (let i = 0; i < bookDB.length; i += 1) {
+    const book = document.createElement('div');
+    book.classList.add('singlebook');
+    book.innerHTML = `
         <div class="book-detail">${bookDB[i].title} by ${bookDB[i].author}</div>
         <button onclick="removeBook('${bookDB[i].title}', '${bookDB[i].author}')">remove</button>
       `;
-      bookshelf.appendChild(book);
-    }
+    bookshelf.appendChild(book);
   }
-  
-  function clickToAdd() {
-    var newTitle = document.getElementById("new-title").value;
-    var newAuthor = document.getElementById("new-author").value;
-    if(newTitle && newAuthor){
-        var newBook = { title: newTitle, author: newAuthor };
-        bookDB.push(newBook);
-        storeBookDB();
-        createBookshelf();
-    }
-    else{
-        createBookshelf();
-    }
-  }
-  
-  function removeBook(title, author) {
-    for (let i = 0; i < bookDB.length; i++) {
-      if (bookDB[i].title === title && bookDB[i].author === author) {
-        bookDB.splice(i, 1);
-        break;
-      }
-    }
+}
+
+function clickToAdd() {
+  const newTitle = document.getElementById('new-title').value;
+  const newAuthor = document.getElementById('new-author').value;
+  if (newTitle && newAuthor) {
+    const newBook = { title: newTitle, author: newAuthor };
+    bookDB.push(newBook);
     storeBookDB();
     createBookshelf();
+  } else {
+    createBookshelf();
   }
+}
 
-  const addButton =  document.getElementById('add');
-  addButton.addEventListener('click', clickToAdd);
-  
-  // Retrieve bookDB from localStorage on page load
-  retrieveBookDB();
-  
-  
+function removeBook(title, author) {
+  for (let i = 0; i < bookDB.length; i += 1) {
+    if (bookDB[i].title === title && bookDB[i].author === author) {
+      bookDB.splice(i, 1);
+      break;
+    }
+  }
+  storeBookDB();
   createBookshelf();
-  
+}
+
+const addButton = document.getElementById('add');
+addButton.addEventListener('click', clickToAdd);
+
+// Retrieve bookDB from localStorage on page load
+retrieveBookDB();
+
+createBookshelf();
