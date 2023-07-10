b8fa733e9e399d92fb6efd4fd442f137307fec8e
AliReza <ali.reza.buet@gmail.com>
2023-07-10 19:17:33 +0600

    Add functionalities by javascript

---
+++ javascripts/bookdata.js
@@ -0,0 +1,21 @@
+export const bookDB = [
+    {
+      title: "Hamlet",
+      author: "Shakespear",
+    },
+    {
+      title: "The Outsider",
+      author: "Albert Camus",
+    },
+    {
+      title: "The Lord of the Rings",
+      author:"J.R.R. Tolkein"
+    },
+    {
+      title: "The Song of Ice and Fire",
+      author: "J.R. Martin"
+    }
+  ]
+  
+  
+  
